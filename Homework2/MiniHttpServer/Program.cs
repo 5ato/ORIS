53d8746c03cbf1ca1c0aa2c7ecd2761af0a9f398
Nail <pomidkil@gmail.com>
2025-10-04 19:02:01 +0300

    first commit

---
+++ Homework2/MiniHttpServer/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Text.Json;
+using MiniHttpServer.Shared;
+
+namespace MiniHttpServer;
+
+public class MyConfiguration
+{
+    public string PublicDirectoryPath { get; set; } = string.Empty;
+    public string Domain { get; set; } = string.Empty;
+    public string Port { get; set; } = string.Empty;
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        string? html = GetPathFile("public/index.html");
+        if (html == null)
+            return;
+
+        using var stream = new FileStream("settings.json", FileMode.OpenOrCreate);
+
+        MyConfiguration? config = JsonSerializer.Deserialize<MyConfiguration>(stream);
+
+        if (config == null)
+        {
+            Console.WriteLine("Empty Json property");
+            return;
+        }
+
+        var httpServer = new HttpServer(config, html);
+        var cts = new CancellationTokenSource();
+
+
+        Task.Run(async () =>
+        {
+            await httpServer.Start(cts.Token);
+        });
+
+        Console.WriteLine("Server started");
+        Console.WriteLine($"http://{config.Domain}:{config.Port}/{config.PublicDirectoryPath}");
+
+        while (true)
+        {
+            var command = Console.ReadLine();
+            if (string.Equals(command, "/stop"))
+            {
+                cts.Cancel();
+                break;
+            }
+        }
+        Console.WriteLine("Server stopped");
+    }
+
+    public static string? GetPathFile(string path)
+    {
+        string? responseText = null;
+        try
+        {
+            responseText = File.ReadAllText(path);
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("File Not Found");
+        }
+        return responseText;
+    }
+}
---
+++ Homework2/MiniHttpServer/Shared/HttpServer.cs
@@ -0,0 +1,55 @@
+using System.Net;
+using System.Text;
+
+namespace MiniHttpServer.Shared;
+
+public class HttpServer(MyConfiguration configuration, string _htmlPage)
+{
+    private readonly MyConfiguration _config = configuration;
+
+    private HttpListener _httpListener;
+
+    public string _htmlPage = _htmlPage;
+
+    public async Task Start(CancellationToken token)
+    {
+        _httpListener = new();
+        _httpListener.Prefixes.Add($"http://{_config.Domain}:{_config.Port}/{_config.PublicDirectoryPath}/");
+        _httpListener.Start();
+
+        await RunningServerAsync(token);
+    }
+
+    private async Task RunningServerAsync(CancellationToken token)
+    {
+        try
+        {
+            while (!token.IsCancellationRequested)
+            {
+                var context = await _httpListener.GetContextAsync();
+
+                var response = context.Response;
+
+                byte[] buffer = Encoding.UTF8.GetBytes(_htmlPage);
+
+                response.ContentLength64 = buffer.Length;
+                using Stream output = response.OutputStream;
+
+                await output.WriteAsync(buffer, token);
+                await output.FlushAsync(token);
+            }
+        }
+        catch (OperationCanceledException)
+        { }
+        finally
+        {
+            Stop();
+        }
+    }
+
+    private void Stop()
+    {
+        _httpListener.Stop();
+        _httpListener.Close();
+    }
+}
